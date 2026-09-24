f9394f3ba9d999df0b9106b54af83d3733eeee1e
Холли <gugurut@inbox.ru>
2026-09-24 21:29:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1922,14 +1922,15 @@ async def check_subscription(callback: types.CallbackQuery):
 
 async def show_instruction_logic(user_id: int):
     instruction_text = premium(
-        "<b>📖 Инструкция по подключению XrayGram</b>\n\n"
-        "<blockquote>"
-        "</b>1. Нажмите кнопку «Подключить»</b>\n"
-        "</b>2. Выберите «Автоматизация чатов»</b>\n"
-        "</b>3. Напишите в поле для ввода: <code>@XrayGramRobot</code></b>"
-        "</blockquote>\n\n"
-        "<b>Разрешения для бота:</b>"
-        "</b>Управление сообщениями 5/5 (для стабильной работы бота)</b>\n\n"
+        instruction_text = premium(
+    "<b>📖 Инструкция по подключению XrayGram</b>\n\n"
+    "<blockquote>"
+    "1. Нажмите кнопку «Подключить»\n"
+    "2. Выберите «Автоматизация чатов»\n"
+    "3. Напишите в поле для ввода: <code>@XrayGramRobot</code>"
+    "</blockquote>\n\n"
+    "<b>Разрешения для бота:</b>\n"
+    "Управление сообщениями 5/5 (для стабильной работы бота)"
     )
     try:
         if os.path.exists(INSTRUCTION_IMAGE_PATH):
